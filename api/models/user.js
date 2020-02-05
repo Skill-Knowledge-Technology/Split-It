eb2dd593b5ffb0cda6505caed1aa0147a7b98e97
Christian Suleiman <cisuleiman@gmail.com>
2020-02-05 16:55:28 -0500

    added User Model

---
+++ api/models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        userID: {
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
+        }, 
+        email : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password : {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+
+    User.associate = function(models) {
+        User.hasMany(models.Transaction)
+    };
+
+    return Userl
+};
