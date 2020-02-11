b7c44bb3cdb87dbbb9ad4ab521d69eb29abf165f
Jimmy Luy <jimmyluy@Jimmys-MacBook-Pro.local>
2020-02-11 18:53:36 -0500

    hello

--- client/src/components/Camera/Camera.js
+++ client/src/components/Camera/Camera.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo-phot'
 
 function Camera(props) {
   return (
     <h1>
-      This is Used For Camera
+      This is Used For Camera - changes done in local
     </h1>
   );
 }
