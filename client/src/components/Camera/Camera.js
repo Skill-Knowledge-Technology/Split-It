298ded58904e0b15edbf645de47ee9ff3118dc6c
Kev <25490322+ChibiKev@users.noreply.github.com>
2020-02-28 15:02:07 -0500

    Added line of code for button and css.

--- client/src/components/Camera/Camera.js
+++ client/src/components/Camera/Camera.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Camera, {FACING_MODES} from 'react-html5-camera-photo';
- 
+import 'react-html5-camera-photo/build/css/index.css';
 import ImagePreview from './ImagePreview'; 
  
 function Cameratake(props) {
