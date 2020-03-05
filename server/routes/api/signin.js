9fc7f295eb367fcd369ab4923ff07f68b7f02f15
Christian Suleiman <cisuleiman@gmail.com>
2020-03-05 13:43:51 -0500

    added function to save new User in signin.js - needs to be tested

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -5,6 +5,7 @@ const router = new express.Router();
 const userController = require('../../controllers/users');
 const {check, param, header} = require('express-validator');
 const validatorErrors = require('../../middleware/validatorErrors');
+const User = require("../../models/User");
 
 router.post('/signup', (req, res, next) => {
     const {body} = req;
@@ -33,5 +34,36 @@ router.post('/signup', (req, res, next) => {
 
 
     // first check that email entered does not already exist, then save user if it is new
-    
+    User.find({
+        email: email
+      }, (err, existingUser) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        } else if (existingUser.length > 0) {
+          return res.send({
+            success: false,
+            message: 'Error: An account with that email already exist.'
+          });
+        }      
+        // Save the new user
+        const newUser = new User();      
+        newUser.email = email;
+        // generate hash of password - defined in User model
+        newUser.password = newUser.generateHash(password);
+        newUser.save((err, user) => {
+          if (err) {
+            return res.send({
+              success: false,
+              message: 'Error: Server error'
+            });
+          }
+          return res.send({
+            success: true,
+            message: 'Account created'
+          });
+        });
+      });
 })
