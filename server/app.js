4d08246d3d62cf16a2804d6f416341c8225dccb7
Christian Suleiman <cisuleiman@gmail.com>
2020-03-05 13:56:42 -0500

    added signup routes to app.js - no erros

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const db = require('./models');
 const app = express();
 const apiRoutes = require('./routes/api/index');
 const userRoutes = require('./routes/api/users');
+const signupRoutes = require('./routes/api/signup');
 
 
 const PORT = process.env.PORT || 8080;
@@ -25,6 +26,7 @@ app.use(morgan(logFormat));
 // Routes
 app.use('/api', apiRoutes);
 app.use('/users', userRoutes);
+app.use('/signup', signupRoutes);
 
 // for production use, we serve the static react build folder
 if(process.env.NODE_ENV==='production') {
