01daf9389f812d7e97eca05a8cec7bd02d811594
ChibiKev <k3v.ch3n@gmail.com>
2020-03-23 17:44:06 -0400

    Step 7 Detailed Split Completed! Added case exception for names to be unique

--- client/src/components/Test/Step7Detailed.js
+++ client/src/components/Test/Step7Detailed.js
@@ -20,7 +20,7 @@ export default class Step7Detailed extends React.Component {
   }
 
   render(){ 
-    const { DetailedSplit} = this.props;
+    const { DetailedSplit } = this.props;
     return(
       <div className="row">
         <div className="col s12 m12 l12">
@@ -33,7 +33,27 @@ export default class Step7Detailed extends React.Component {
               </button>
             </div>
             <div className="card-content white-text">
-              <h1>Detailed: Step 7</h1>
+              <table className="highlight centered">
+                <thead>
+                  <tr>
+                    <th>Names</th>
+                    <th>Payment</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {DetailedSplit.names.map((list, index) => (
+                    <tr key = {index}>
+                      <td>
+                        {list.name}
+                      </td>
+                      <td>
+                        $ {list.cost}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <br/>
               <button className="btn waves-effect waves-light float-right"
                 type="submit" name="action" onClick = {this.save}>
                 Save
