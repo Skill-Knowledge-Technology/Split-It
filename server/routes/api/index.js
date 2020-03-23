bd297ba96c8b7c393d706970cae83389f2996012
jimmyluy1 <luyjimmy037@gmail.com>
2020-03-23 19:34:05 -0400

    added transaction route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = new express.Router();
 const users = require('./users');
+const transactions = require('./tranactions');
 
   router.use('/users', users);
 
   // ../api/
- 
+ router.use('/transactions',transactions);
     
   router.use('*', (req, res, next) => {
       res.status(404).json({errors: [{msg: 'Unknown API route'}]});
---
+++ server/routes/api/transactions.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = new express.Router;
+const transactionController = require('../../controllers/transactions');
+const {check, param, header} = require('express-validator');
+const validatorErrors = require('../../middleware/validatorErrors');
+const transactionMiddleware = require('../../middleware/treansactions')
+
+
+//should match with '/api/transactions'
+router.get('/:transactionId', [validatorErrors],transactionController.getTransId);
+
