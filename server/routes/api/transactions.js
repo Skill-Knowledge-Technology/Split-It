bcb4e49d242c01e1b7f6c28ea5b055f828f0dbb9
jimmyluy1 <luyjimmy037@gmail.com>
2020-03-23 20:06:45 -0400

    exported function

--- server/routes/api/transactions.js
+++ server/routes/api/transactions.js
@@ -3,9 +3,10 @@ const router = new express.Router;
 const transactionController = require('../../controllers/transactions');
 const {check, param, header} = require('express-validator');
 const validatorErrors = require('../../middleware/validatorErrors');
-const transactionMiddleware = require('../../middleware/treansactions')
+const transactionMiddleware = require('../../middleware/transactions')
 
 
 //should match with '/api/transactions'
 router.get('/:transactionId', [validatorErrors],transactionController.getTransId);
 
+module.exports = router;
