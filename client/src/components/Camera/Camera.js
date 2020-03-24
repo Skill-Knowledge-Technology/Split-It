55afa2833f1f3b162784b63c17eff8e2823d6bbf
ChibiKev <k3v.ch3n@gmail.com>
2020-03-24 09:54:25 -0400

    Worked on Camera, using tesseract

--- client/src/components/Camera/Camera.js
+++ client/src/components/Camera/Camera.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import ImageUploader from 'react-images-upload';
  
-class Camera extends React.Component {
+export default class Camera extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pictures: [] };
+    this.onDrop = this.onDrop.bind(this);
+  }
  
-    constructor(props) {
-        super(props);
-         this.state = { pictures: [] };
-         this.onDrop = this.onDrop.bind(this);
-    }
- 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
-        });
-    }
- 
-    render() {
-        return (
-            <ImageUploader
-                withIcon={true}
-                withPreview={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png']}
-                maxFileSize={5242880}
-            />
-        );
-    }
+  onDrop(picture) {
+    this.setState({
+      pictures: this.state.pictures.concat(picture),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ImageUploader
+          withIcon={true}
+          withPreview={true}
+          buttonText='Choose images'
+          onChange={this.onDrop}
+          imgExtension={['.jpg', '.gif', '.png']}
+          maxFileSize={5242880}
+        />
+        <div className="center-align">
+          <a className="btn-large waves-effect waves-light" href="Test">
+            In Testing
+            <i className="material-icons right">cached</i>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
-export default Camera;
