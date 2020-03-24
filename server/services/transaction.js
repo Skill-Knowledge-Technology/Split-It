b6356e05540f65974ddfd36a8bc2f56a0aa822bd
jimmyluy1 <luyjimmy037@gmail.com>
2020-03-24 17:17:32 -0400

    create transaction in services

--- server/services/transaction.js
+++ server/services/transaction.js
@@ -5,8 +5,18 @@ const findTransaction = async (id) => {
     console.log(Transaction + 'found');
     return Transaction;
 }
+const createTransaction = async (data)=> {
+    const newTransaction = await db.Transaction.create({
+        balance: data.balance,
+        ownerId: data.ownerId
+    });
+    console.log("service has creaetd a transaction owned by user " + newTransaction.ownerId);
+    return newTransaction;
+
+}
 
 
 module.exports ={
-    findTransaction
+    findTransaction,
+    createTransaction
 }
