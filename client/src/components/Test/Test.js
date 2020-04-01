9d4bd22a6a6fbc6930acb47ce010609838f35884
ChibiKev <k3v.ch3n@gmail.com>
2020-03-31 21:46:49 -0400

    Modified Camera, Using same format as manual input

--- client/src/components/Test/Test.js
+++ client/src/components/Test/Test.js
@@ -68,53 +68,59 @@ export default class Test extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        { /* File uploader */ }
-        <section className="hero">
-          <label className="fileUploaderContainer">
-            Click here to upload documents
-            <input type="file" id="fileUploader" onChange={this.handleChange} multiple />
-          </label>
-
-          <div>
-            { this.state.uploads.map((value, index) => {
-              return <img key={index} src={value} width="100px" />
-            }) }
-          </div>
-
-          <button onClick={this.generateText} className="button">Generate</button>
-        </section>
-
-        { /* Results */ }
-        <section className="results">
-          { this.state.documents.map((value, index) => {
-            return (
-              <div className="container TestBox">
-                <div key={index} className="results__result">
-                  <div className="results__result__image">
-                    <img src={this.state.uploads[index]} width="250px" />
-                  </div>
-                  <div className="results__result__info">
-                    <div className="results__result__info__codes">
-                      <small><strong>Confidence Score:</strong> {value.confidence}</small>
+      <div className = "container">
+        <div className="row">
+          <div className="col s12 m12 l12">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <div className = "container">
+                  <div className="file-field input-field">
+                    <div className="btn">
+                      <input type="file" onChange={this.handleChange} accept="image/*"/>
+                      <span> Upload File</span>
                     </div>
-                    <div className="results__result__info__codes">
-                      <small><strong>Pattern Output:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
-                    </div>
-                    <div className="results__result__info__text">
-                      <small><strong>Full Output:</strong> {value.text}</small>
+                    <div className="file-path-wrapper">
+                      <input className="file-path validate" type="text"/>
                     </div>
                   </div>
+                  <div>
+                    {this.state.uploads.map((value, index) => {
+                      return <img key={index} src={value} width="100px" />
+                    })}
+                  </div>
+                  <button className="btn waves-effect waves-light" type="submit" onClick={this.generateText}>Submit
+                    <i className="material-icons right">send</i>
+                  </button>
+                  {this.state.documents.map((value, index) => {
+                    return (
+                      <div className="container">
+                        <div key={index} className="results__result">
+                          <div className="results__result__image">
+                            <img src={this.state.uploads[index]} width="250px" />
+                          </div>
+                          <div className="results__result__info">
+                            <div className="results__result__info__codes">
+                              <small><strong>Confidence Score:</strong> {value.confidence}</small>
+                            </div>
+                            <div className="results__result__info__codes">
+                              <small><strong>Pattern Output:</strong> {value.pattern.map((pattern) => { return pattern + ', ' })}</small>
+                            </div>
+                            <div className="results__result__info__text">
+                              <small><strong>Full Output:</strong> {value.text}</small>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
-            )
-          }) }
-        </section>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
-
-
   // constructor(props) {
   //   super(props);
   //   this.state = {
