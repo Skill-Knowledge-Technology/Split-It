4bfbc6c5c4d14aa4c8c8b98f63a3e369cf1109b1
Christian Suleiman <cisuleiman@gmail.com>
2020-04-20 20:40:24 -0400

    tried to run migrations

--- server/migrations/20200420233046-friendship table.js
+++ server/migrations/20200420233046-friendship table.js
@@ -3,27 +3,36 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Friendship', {
+      // User sending friend request
       requesterID: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER,
         allowNull: false
       },
 
+      // User receiving friend request
       addresseeID: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.DataTypes.INTEGER,
         allowNull: false
       },
 
-      
-    })
+      // Primary key of this table
+      friendshipID: {
+        type: (this.requesterID, this.addresseeID),
+        primaryKey: true
+      },
+
+      // 0 = blocked, 1 = pending, 2 = friends
+      friendshipStatus: {
+        type: Sequelize.Datatypes.INTEGER,
+        allowNull: false,
+        validate: {
+          equals: 0 || 1 || 2
+        }
+      }
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('Friendship');
   }
 };
