a3f78077a547ed38b72049bf26119405d568afc9
ChibiKev <k3v.ch3n@gmail.com>
2020-04-25 19:57:37 -0400

    Changed Sidebar

--- client/src/components/Sidebar/ProfileLogOutRedirect.js
+++ client/src/components/Sidebar/ProfileLogOutRedirect.js
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom';
 
 // this will show the profile and logout option if user is authenticated
 const ProfileLogOutRedirect = ({ userID, name, handleUserLogOut }) => {
-    return (
-        <div>
-            <li>  
-                <Link to={`/Profile/${userID}`} className="waves-effect"> {name} </Link>
+  return (
+    <div>
+      <li>  
+        <Link to={`/Profile/${userID}`} className="waves-effect"> {name} </Link>
 
-            </li>
-            <li>  
-                <Link to={`/Payments/${userID}`} className="waves-effect"> Payments </Link>
+      </li>
+      <li>  
+        <Link to={`/Payments/${userID}`} className="waves-effect"> Payments </Link>
 
-            </li>
-            <li>
-                <Link to="/" onClick={handleUserLogOut} className="waves-effect">Logout</Link>
-            </li>
-        </div>
-    )
+      </li>
+      <li>
+        <Link to="/" onClick={handleUserLogOut} className="waves-effect">Logout</Link>
+      </li>
+    </div>
+  )
 }
 
 export default ProfileLogOutRedirect;
-
