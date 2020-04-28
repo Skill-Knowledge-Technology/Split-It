dddb3bb97f2fd7b5f93d496945d7a89190117e2c
ChibiKev <k3v.ch3n@gmail.com>
2020-04-28 13:06:39 -0400

    Changed Profile Page, Allow Edits

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -9,6 +9,7 @@ class Profile extends React.Component {
       name: this.props.name,
       email: this.props.email,
       balance: this.props.balance,
+      uploads: require("../../public/People/dummy.jpg"),
       edit: false,
     };
   }
@@ -17,6 +18,25 @@ class Profile extends React.Component {
   handleChange = (input) => (e) => {
     this.setState({ [input]: e.target.value });
   };
+
+  handleImageChange = (e) => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+
+    if (e.target.files.length === 0) {
+      return;
+    }
+
+    reader.onloadend = (e) => {
+      this.setState({
+        uploads: [reader.result],
+        text: '',
+        failAttempts: 0,
+        found: false,
+      });
+    }
+    reader.readAsDataURL(file);
+  }
 
   // eventually api call to call the backend
   handleSubmit = (e) => {
@@ -28,11 +48,10 @@ class Profile extends React.Component {
   handleEdit = (e) => {
     e.preventDefault();
     this.setState({ edit: !this.state.edit});
-    console.log(this.state.edit)
   };
   
   render() {
-    const {name, email, balance } = this.props;
+    const {name, email, balance, uploads} = this.state;
     return (
       <div className="container">
         <div className="row">
@@ -40,31 +59,90 @@ class Profile extends React.Component {
             <div className="card blue-grey darken-1">
               <div className="card-content white-text">
                 <span className="card-title">Profile</span>
-                <div className="container">
-                  <div className="center-align">
-                    <img src={require("../../public/People/dummy.jpg")} width="200" className="circle responsive-img" alt="Empty"/>
+                {!this.state.edit && 
+                  (
+                  <div className="container">
+                    <div className="center-align">
+                      <img src={uploads} width="200" className="circle responsive-img" alt="Empty"/>
+                    </div>
+                    <div className="row">
+                      <label for="username" className="active">Username: </label>
+                      <i className="material-icons left">account_box</i>
+                      <span id="username"> {name}</span>
+                    </div>
+                    <div className="row">
+                      <label for="email" className="active">E-mail: </label>
+                      <i className="material-icons left">email</i>
+                      <span id="email"> {email}</span>
+                    </div>
+                    <div className="row">
+                      <label for="balance" className="active">Current Balance: </label>
+                      <i className="material-icons left">account_balance_wallet</i>
+                      <span for="balance"> ${balance}</span>
+                    </div>
+                    <div className="row">
+                      <div className="left">
+                        <button className="btn" type="edit" name="action"
+                          onClick={this.handleEdit}>
+                          Edit
+                          <i className="material-icons right">edit</i>
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="row">
-                    <label for="username" className="active">Username: </label>
-                    <i className="material-icons left">account_box</i>
-                    <span id="username"> {name}</span>
+                )}
+                {this.state.edit && 
+                  (
+                  <div className="container">
+                    <div className="center-align">
+                      <div>
+                        <img src={uploads} width="200" className="circle responsive-img" alt="Empty"/>
+                      </div>
+                      <div>
+                        <input type="file" onChange={this.handleImageChange} accept="image/*"/>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <label for="username" className="active">Username: </label>
+                      <i className="material-icons left">account_box</i>
+                      <span id="username"> {name}</span>
+                    </div>
+                    <form>
+                      <div className="row">
+                        <div className="input-field col s12">
+                          <i className="material-icons prefix">email</i>
+                          <label for="email" className="active">E-mail</label>
+                          <input id="email" type="email" placeholder="Enter Email" className="validate"
+                            value={email} onChange={this.handleChange("email")}/>
+                          <span className="helper-text" data-error="Invalid Email" data-success="Valid">
+                            Please Enter a Valid Email
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                    <div className="row">
+                      <label for="balance" className="active">Current Balance: </label>
+                      <i className="material-icons left">account_balance_wallet</i>
+                      <span for="balance"> ${balance}</span>
+                    </div>
+                    <div className="row">
+                      <div className="left">
+                        <button className="btn" type="submit" name="action"
+                          onClick={this.handleSubmit}>
+                          Save
+                          <i className="material-icons right">save</i>
+                        </button>
+                        </div>
+                        <div className="right">
+                        <button className="btn" type="edit" name="action"
+                          onClick={this.handleEdit}>
+                          Cancel
+                          <i className="material-icons right">cancel</i>
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="row">
-                    <label for="email" className="active">E-mail: </label>
-                    <i className="material-icons left">email</i>
-                    <span id="email"> {email}</span>
-                  </div>
-                  <div className="row">
-                    <label for="balance" className="active">Current Balance: </label>
-                    <i className="material-icons left">account_balance_wallet</i>
-                    <span for="balance"> ${balance}</span>
-                  </div>
-                  <button className="btn" type="edit" name="action"
-                    onClick={this.handleEdit}>
-                    Edit
-                    <i className="material-icons right">edit</i>
-                  </button>
-                </div>
+                )}
               </div>
             </div>
           </div>
